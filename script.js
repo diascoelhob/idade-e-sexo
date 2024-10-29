59e73318b2a3896c96743061af5afdb8e05650c1
diascoelhob <diascoelhob@gmail.com>
2024-10-28 22:38:35 -0300

    salvando

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ function verificar() {
     var agora_ano = (new Date).getFullYear()
     var idade = agora_ano - nascimento
 
+    var img = document.querySelector('div#imagem')
+
     var sexo = document.getElementsByName('sexo')
     if (sexo[0].checked) {
-        sexo = 'M'
+        if (idade < 12) {
+            img.innerHTML = '                <img src="imagens/garoto.jpg" alt="garoto">'
+        } else if (idade < 50) {
+            img.innerHTML = '                <img src="imagens/homem.jpg" alt="homem">'
+        } else if (idade < 110) {
+            img.innerHTML = '                <img src="imagens/idoso.jpg" alt="idoso">'
+        }
     } else if (sexo[1].checked) {
-        sexo = 'F'
+        if (idade < 12) {
+            img.innerHTML = '                <img src="imagens/garota.jpg" alt="garota">'
+        } else if (idade < 50) {
+            img.innerHTML = '                <img src="imagens/mulher.jpg" alt="mulher">'
+        } else if (idade < 110) {
+            img.innerHTML = '                <img src="imagens/idosa.jpg" alt="idosa">'
+        }
     }
 }
